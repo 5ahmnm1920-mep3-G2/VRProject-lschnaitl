754cd92a5fe5ea00e57eee3bf37a2c210ec6ce4e
RIPinPuppers <leonhard.schnaitl@htl-salzburg.ac.at>
2020-01-20 14:13:57 +0100

    added shooting mechanic and building up force when holding down shoot button

--- Assets/_MyGame/Scripts/ShootPlanet.cs
+++ Assets/_MyGame/Scripts/ShootPlanet.cs
@@ -4,6 +4,7 @@ public class ShootPlanet : MonoBehaviour
 {
     [SerializeField] private Transform planet = null;
     [SerializeField] private Gravitation gravitation;
+    [SerializeField] private float factorIncreace = 0.2f;
 
     private Rigidbody planetRb;
     private bool isReset;
@@ -22,18 +23,33 @@ public class ShootPlanet : MonoBehaviour
             ResetPlanetPosition();
         }
 
-        if (Input.GetKey(actionKey))
+        if (Input.GetKey(actionKey) && isReset)
         {
-            forceFactor += 0.1f;
+            forceFactor += factorIncreace * Time.deltaTime;
+            Debug.Log(forceFactor);
+        }
+        else if (Input.GetKeyUp(actionKey))
+        {
+            Shoot(forceFactor);
         }
     }
-
+    
     private void ResetPlanetPosition()
     {
         gravitation.EarthAttract(false);
-        planetRb.MovePosition(transform.position);
+        planet.position = transform.position;
+        planet.rotation = transform.rotation;
         planet.parent = transform;
+        planetRb.velocity = Vector3.zero;
         isReset = true;
         forceFactor = 0;
+    }
+
+    private void Shoot(float zForce)
+    {
+        planet.parent = null;
+        gravitation.EarthAttract(true);
+        isReset = false;
+        planetRb.AddForce(transform.forward.normalized * zForce);
     }
 }
